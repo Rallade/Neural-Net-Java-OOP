b5ab18d88e9998c8db8721666d1cabf102973bf6
Rallade <rallade@hotmail.com>
2017-02-06 16:20:30 +0000

    Graph object
    Created Graph object

---
+++ src/graph/Graph.java
@@ -0,0 +1,59 @@
+package graph;
+
+import java.util.ArrayList;
+
+public class Graph {
+	private int e, v;
+	private ArrayList<Edge> edges;
+	private ArrayList<Vertex> verteces;
+
+	public Graph(int e, int v) {
+		this.e = e;
+		this.v = v;
+	}
+
+	public int getE() {
+		return e;
+	}
+
+	public int getV() {
+		return v;
+	}
+
+	public ArrayList<Edge> getEdges() {
+		return edges;
+	}
+
+	public ArrayList<Vertex> getVerteces() {
+		return verteces;
+	}
+	
+	public void createVertex(String label){
+		verteces.add(new Vertex(label));
+	}
+	
+	public void createVertex(){
+		verteces.add(new Vertex());
+	}
+	
+	public void creatVertexPair(String label1, String label2){
+		Vertex a  = new Vertex(label1);
+		Vertex b = new Vertex(label2);
+		verteces.add(a);
+		verteces.add(b);
+		edges.add(new Edge(a,b));
+	}
+	
+	public void creatVertexPair(){
+		Vertex a  = new Vertex();
+		Vertex b = new Vertex();
+		verteces.add(a);
+		verteces.add(b);
+		edges.add(new Edge(a,b));
+	}
+	
+	public void link(Vertex a, Vertex b){
+		 edges.add(new Edge(a,b));
+	}
+	
+}
